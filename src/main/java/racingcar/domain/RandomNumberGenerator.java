12ea327eb4711fcbf2cbac7f18d760fb6b9a957b
iamjunsulee <mellow0924@gmail.com>
2022-04-21 23:05:13 +0900

    fix(RandomNumberGenerator): 최대값 오류 수정

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
     public static final int MIN_BOUND = 0;
-    public static final int MAX_BOUND = 10;
+    public static final int MAX_BOUND = 9;
 
     public int generate() {
         return Randoms.pickNumberInRange(MIN_BOUND, MAX_BOUND);
